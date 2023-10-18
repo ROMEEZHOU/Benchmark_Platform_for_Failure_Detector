f68e41bad26fcdeb5f483406ad89af77c1e1118c
I741907 <yuchen.zhou02@sap.com>
2023-10-18 12:58:43 +0800

    write data_processing, but have errors

--- Extension/data_processing.py
+++ Extension/data_processing.py
@@ -49,47 +49,131 @@ For the traces that node 10 received, they are generated from the traces that no
 For the traces that node 10 received from node 1, they are generated from traces node 1 received from node 0, with some noices
 '''
 
-def add_node(data_file, permute_number, permute_list, permute_index, node_number):
+def add_node(data_file, permute_number, permute_list, permute_index, backup_permute_list, backup_permute_index, node_number):
     original_list = [0,1,3,5,6,7,8,9]
     copied_node_number = original_list[(node_number - 2) % 8]
+    copied_node_path = os.path.join(data_file, 'Node'+str(copied_node_number))
     '''the data misses node 2 and node 4'''
 
     '''first step, for other node, generate trace10.csv from trace0.csv'''
-    directories = []
     for item in os.listdir(data_file):
         if item != 'Node' + str(copied_node_number):
             csv_path = os.path.join(data_file, item, 'trace' + str(copied_node_number) + '.csv')
 
             df = pd.read_csv(csv_path, usecols=['site', 'timestamp_send', 'timestamp_receive'])
             first_timestamp_send, first_timestamp_receive = df.loc[0, ['timestamp_send', 'timestamp_receive']]
-            df_diff = df.diff()
+            df_diff = df.diff()[1:]
+            
+            new_trace = pd.DataFrame(data = {'site': [node_number], 'timestamp_send': [first_timestamp_send], 'timestamp_receive': [first_timestamp_receive]})
+            total_length, _ = df_diff.shape
+            sub_length = total_length // permute_number
+            for per_index in permute_list[permute_index]:
+                if per_index != permute_number:
+                    new_trace = pd.concat([new_trace, df_diff.iloc[((per_index-1) * sub_length):((per_index*sub_length) + 1),:]])
+                else:
+                    new_trace = pd.concat([new_trace, df_diff.iloc[(total_length - (permute_number-1)*sub_length):,:]])
+
+            '''write the new trace'''
+            new_trace = new_trace.cumsum()
+            new_trace.to_csv(path_or_buf=os.path.join(data_file, item, 'trace' + str(node_number) + '.csv'), index=False)
+
+
+        else:
+            '''if the node is the copied node itself, copy the traces it received from Node copied_node_number+1 as the traces it received from Node node_number'''
+
+            backup_copied_node_number = original_list[(node_number - 1) % 8]
+            csv_path = os.path.join(data_file, item, 'trace' + str(backup_copied_node_number) + '.csv')
+
+            df = pd.read_csv(csv_path, usecols=['site', 'timestamp_send', 'timestamp_receive'])
+            first_timestamp_send, first_timestamp_receive = df.loc[0, ['timestamp_send', 'timestamp_receive']]
+            df_diff = df.diff()[1:]
+
+            new_trace = pd.DataFrame(data = {'site': [1], 'timestamp_send': [first_timestamp_send], 'timestamp_receive': [first_timestamp_receive]})
+            total_length, _ = df_diff.shape
+            sub_length = total_length // (permute_number+1)
+            for per_index in backup_permute_list[backup_permute_index]:
+                if per_index != permute_number+1:
+                    new_trace = pd.concat([new_trace, df_diff.iloc[((per_index-1) * sub_length):((per_index*sub_length) + 1),:]])
+                else:
+                    new_trace = pd.concat([new_trace, df_diff.iloc[(total_length - (permute_number-1)*sub_length):,:]])
+            new_trace = new_trace.cumsum()
+            new_trace.to_csv(path_or_buf=os.path.join(data_file, item, 'trace' + str(node_number) + '.csv'), index=False)
+
+
+    '''second step, for node10, generate tracex.csv from tracex.csv in Node 0'''
+    add_node_path = os.path.join(data_file, 'Node'+str(node_number))
+    os.makedirs(add_node_path, exist_ok=True)
+    for item in os.listdir(copied_node_path):
+        csv_path = os.path.join(copied_node_path, item)
+
+        df = pd.read_csv(csv_path, usecols=['site', 'timestamp_send', 'timestamp_receive'])
+        first_timestamp_send, first_timestamp_receive = df.loc[0, ['timestamp_send', 'timestamp_receive']]
+        df_diff = df.diff()[1:]
+
+        site = str(item).split('.')[0][5]
+        site = int(site)
+        new_trace = pd.DataFrame(data = {'site': [site], 'timestamp_send': [first_timestamp_send], 'timestamp_receive': [first_timestamp_receive]})
+        total_length, _ = df_diff.shape
+        sub_length = total_length // permute_number
+        for per_index in permute_list[permute_index]:
+            if per_index != permute_number:
+                new_trace = pd.concat([new_trace, df_diff.iloc[((per_index-1) * sub_length):((per_index*sub_length) + 1),:]])
+            else:
+                new_trace = pd.concat([new_trace, df_diff.iloc[(total_length - (permute_number-1)*sub_length):,:]])
+        new_trace = new_trace.cumsum()
+        new_trace.to_csv(path_or_buf=os.path.join(add_node_path, item), index=False)
+
+    '''now generate the trace node10 received from the node it copies'''
+    backup_copied_node_number = original_list[(node_number + 1) % 8]
+    csv_path = os.path.join(data_file, 'Node'+str(backup_copied_node_number), 'trace'+str(copied_node_number)+'.csv')
+
+    df = pd.read_csv(csv_path, usecols=['site', 'timestamp_send', 'timestamp_receive'])
+    first_timestamp_send, first_timestamp_receive = df.loc[0, ['timestamp_send', 'timestamp_receive']]
+    df_diff = df.diff()[1:]
+
+    new_trace = pd.DataFrame(data = {'site': [copied_node_number], 'timestamp_send': [first_timestamp_send], 'timestamp_receive': [first_timestamp_receive]})
+    total_length, _ = df_diff.shape
+    sub_length = total_length // (permute_number+1)
+    for per_index in backup_permute_list[backup_permute_index]:
+        if per_index != permute_number+1:
+            new_trace = pd.concat([new_trace, df_diff.iloc[((per_index-1) * sub_length):((per_index*sub_length) + 1),:]])
+        else:
+            new_trace = pd.concat([new_trace, df_diff.iloc[(total_length - (permute_number-1)*sub_length):,:]])
+    new_trace = new_trace.cumsum()
+    new_trace.to_csv(path_or_buf=os.path.join(add_node_path, 'trace'+str(copied_node_number)+'.csv'), index=False)
 
     return
 
 '''
 main function for controlling data processing, the number n is the number of nodes we want to add to existing traces
 '''
-def data_processing(n, data_file):
+def add_data(n, data_file='../data2'):
     added_node = 0
     permute_number = 3
     permute_list = permute(permute_number)
+    backup_permute_list = permute(permute_number + 1)
     permute_upper = added_node + len(permute_list)
     permute_index = 0
+    backup_permute_index = 0
     node_number = 10
 
     while added_node < n:
         if added_node >= permute_upper:
             permute_number += 2
             permute_list = permute(permute_number)
+            backup_permute_list = permute(permute_number+1)
+            permute_index = 0
+            backup_permute_index = 0
             permute_upper += len(permute_list)
 
-        add_node(data_file = data_file, permute_number = permute_number, permute_list = permute_list, permute_index = permute_index, node_number = node_number)
+        add_node(data_file = data_file, permute_number = permute_number, permute_list = permute_list, permute_index = permute_index, backup_permute_list = backup_permute_list, backup_permute_index = backup_permute_index, node_number = node_number)
         last_round = added_node // 8
         added_node += 1
         node_number += 1
         current_round = added_node // 8
         if current_round > last_round:
             permute_index += 1
+            backup_permute_index += 1
 
 
 '''The function to re-arrange the original Node traces into pairs, e.g. under data2/Node0, we have trace1.csv for the heartbeats received from Node1'''
@@ -111,5 +195,4 @@ def first_operation():
                 df.to_csv(path_or_buf=os.path.join('../data2', i, 'trace' + j[4:] + '.csv'), index=False)
 
 if __name__ == '__main__':
-    #first_operation()
-    pass
+    add_data(1,'..\data2')
